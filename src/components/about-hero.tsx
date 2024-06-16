2f474b9485db77864d657035d014bc926b276bf9
Ayushatre05 <ayushkatre1801@gmail.com>
2024-06-16 09:12:59 +0530

    added components

---
+++ src/components/about-hero.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+
+import { AnimatePresence } from "framer-motion";
+
+import FadeUp from "@/animation/fade-up";
+import FadeRight from "@/animation/fade-right";
+import heroProfileImg from "@/public/images/Hackathon.jpg";
+
+export default function AboutHero() {
+  return (
+    <div className="mx-auto mt-0 flex max-w-7xl flex-col items-center gap-6 px-6 pt-20 text-center sm:px-14 md:mt-20 md:px-20 lg:mt-0 lg:flex-row lg:text-left">
+      <div className="w-full sm:w-1/2 md:w-2/3 lg:inline-block lg:h-full lg:w-1/2">
+        <AnimatePresence>
+          <FadeUp key="hero-image" duration={0.6}>
+            <Image
+              src={heroProfileImg}
+              width={100}
+              height={100}
+              className="h-auto w-full px-0 xl:px-16"
+              alt="hero image"
+              unoptimized
+            />
+          </FadeUp>
+        </AnimatePresence>
+      </div>
+      <div className="sm:1/2 mt-10 w-full lg:w-1/2">
+        <AnimatePresence>
+          <FadeUp key="title-greeting" duration={0.6}>
+            <h1 className="text-6xl font-bold text-accent sm:text-7xl md:text-6xl lg:text-5xl xl:text-7xl">
+              Hi, I&apos;m Ayush
+            </h1>
+          </FadeUp>
+          <FadeUp key="description-1" duration={0.6} delay={0.2}>
+            <p className="mt-8 text-base font-medium text-zinc-900 dark:text-zinc-300 sm:text-lg md:text-lg">
+              A hobbyist Web Application Developer currently diving deep into
+              MERN Stack. I excel in navigating the realms of databases,
+              server-side logic, and creating seamless user interfaces. My
+              proficiency in MERN Stack allows me to develop robust, scalable,
+              and efficient web applications. Additionally, I am skilled in
+              TypeScript and Next.js, enabling me to enhance code quality and
+              performance while implementing modern, server-rendered
+              applications. I am adept at handling both the back-end and
+              front-end components, ensuring a smooth and cohesive user
+              experience.
+            </p>
+          </FadeUp>
+          <FadeUp key="description-2" duration={0.6} delay={0.4}>
+            <p className="mt-8 text-base font-medium text-zinc-900 dark:text-zinc-300 sm:text-lg md:text-lg">
+              Explore my latest projects showcasing my expertise in Reactjs,
+              Nextjs, Javascript, Typescript and web development.
+            </p>
+          </FadeUp>
+          <FadeRight
+            key="hero-location"
+            duration={0.6}
+            delay={0.8}
+            className="mr-0 mt-8 flex items-center justify-center gap-4 lg:mr-8 lg:justify-end"
+          >
+            <div className="relative flex w-12 gap-4 overflow-hidden rounded-md">
+              <Image
+                className="-z-10 h-full w-full bg-cover bg-no-repeat"
+                alt="Indian flag"
+                src="https://flagcdn.com/in.svg"
+                width={15}
+                height={15}
+              />
+            </div>
+            <span className="text-lg font-medium text-foreground">
+              Pune, India
+            </span>
+          </FadeRight>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+}
