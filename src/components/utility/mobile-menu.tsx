11c8e29654e028279382ffc5520c3f0754aff780
Ayushatre05 <ayushkatre1801@gmail.com>
2024-08-03 19:55:42 +0530

    changes

--- src/components/utility/mobile-menu.tsx
+++ src/components/utility/mobile-menu.tsx
@@ -58,7 +58,7 @@ export default function MobileMenu({
                 ))}
                 <ThemeSwitch setClose={setOpenMenu} />
               </div>
-              <div className="absolute bottom-0 py-6">©2024 Ashish Bhosle</div>
+              <div className="absolute bottom-0 py-6">©2024 Ayush Katre</div>
             </Dialog.Panel>
           </Transition.Child>
         </div>
