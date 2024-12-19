f1513178be8384b420320a915fcf1bab2ccc0b68
Ayush Katre <ayushkatre1801@gmail.com>
2024-12-19 22:12:55 +0530

    fix bug

--- src/data/siteMetaData.mjs
+++ src/data/siteMetaData.mjs
@@ -5,6 +5,14 @@ export const siteMetadata = {
   description: "NextJs developer",
   language: "en-us",
   theme: "light",
+  siteName: "ayush.me",
+  
   siteRepo: "https://github.com/AyushKatre05",
   siteLogo: "/favicon.ico",
+  email: "ayushkatre1801@gmail.com",
+  github: "https://github.com/AyushKatre05",
+  twitter: "https://twitter.com/ayush_katre_05",
+  linkedin: "https://www.linkedin.com/in/ayush-katre",
+  locale: "en-US",
+  googleSiteVerification: "",
 };
